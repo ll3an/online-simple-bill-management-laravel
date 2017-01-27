bda4df7fe699dd42e7d5372404405f5a842997a6
NH Evan <nhevan@gmail.com>
2017-01-27 18:20:40 +0600

    Create customer model and factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Customer::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->phoneNumber,
+        'short_address' => $faker->streetAddress,
+        'detail_address' => $faker->address
+    ];
+});
