2e17a23df86f3980aa7ccd0fe1364a9510eab3ec
NH Evan <nhevan@gmail.com>
2017-01-27 15:11:29 +0600

    Create migration file for customers table and migrate

---
+++ database/migrations/2017_01_27_090648_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('phone');
+            $table->string('email');
+            $table->text('short_address');
+            $table->text('detail_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
