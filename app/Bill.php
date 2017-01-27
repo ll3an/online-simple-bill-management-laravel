99b05928e254e8ca1038cdb045cdd6b1e26e2824
NH Evan <nhevan@gmail.com>
2017-01-27 22:47:37 +0600

    Bill model belongs to a Customer and can have many payments associated with it.

--- app/Bill.php
+++ app/Bill.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bill extends Model
 {
-    //
+    /**
+     * A Bill belongs to a customer
+     * @return belongsTo Customer
+     */
+    public function customer()
+    {
+    	return $this->belongsTo('App\Customer');
+    }
+
+    /**
+     * A bill instance can have multiple payments associated with it
+     * @return hasMany payments
+     */
+    public function payments()
+    {
+    	return $this->hasMany('App\Payment');
+    }
 }
