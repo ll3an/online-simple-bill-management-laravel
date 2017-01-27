9f1259924ad157cff85f54f8262d2921dd6fd513
NH Evan <nhevan@gmail.com>
2017-01-27 23:08:46 +0600

    Payments belongs to Bills

--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    /**
+     * a payment belongs to a bill
+     * @return belongsTo Bill
+     */
+    public function bill()
+    {
+    	return $this->belongsTo('App\Bill');
+    }
 }
