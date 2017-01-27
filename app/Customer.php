e06ca3eedcc61ceb1e6855c8152a4fd012bd0774
NH Evan <nhevan@gmail.com>
2017-01-27 19:53:37 +0600

    Customers can now have many bills and payments.

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+	/**
+	 * customer has many bills
+	 * @return hasMany relation object
+	 */
+    public function bills()
+    {
+    	return $this->hasMany('App\Bill');
+    }
+
+    /**
+     * customer has many payments
+     * @return hasMany relation object
+     */
+    public function payments()
+    {
+    	return $this->hasMany('App\Payment');
+    }
 }
