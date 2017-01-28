9784bf10bb013e73e16409b3e4ff5fb8fa83a019
NH Evan <nhevan@gmail.com>
2017-01-28 22:51:32 +0600

    Update dashboard view with anchor links to related resources.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-4 col-md-offset-4">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading text-center">Actions</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <a href='{{ url('/new-bill-form') }}' class="btn btn-primary btn-block">New Bill</a>
+                    <a href='{{ url('/all-due') }}' class="btn btn-danger btn-block">All Due</a>
+                    <a href='{{ url('/all-paid') }}' class="btn btn-success btn-block">All Paid</a>
+                    <a href='{{ url('/customers') }}' class="btn btn-warning btn-block">Customers</a>
                 </div>
             </div>
         </div>
